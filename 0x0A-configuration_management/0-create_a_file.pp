ebc03b9095ffb1b0c46681c46b1b1d7a66f21fc1
coosoti <charlesosoti@gmail.com>
2021-08-13 17:46:33 +0300

    [Fix] added a puppet scriptto create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # create a file in /tmp
 file { 'tmp/holberton':
   ensure  => file,
-  path    => 'tmp/holberton',
+  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
