7a9395d82343ac4c45841ca936337b37a5aeeaa4
coosoti <charlesosoti@gmail.com>
2021-08-17 22:46:27 +0300

    added puppet manifest to install and configure nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+# Puppet manifest to install nginx
+# Nginx should be listening on port 80
+# When querying Nginx at its root / with a GET request
+# (requesting a page) using curl, it must return a page that
+# contains the string Holberton School
+# The redirection must be a “301 Moved Permanently”
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { '':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
