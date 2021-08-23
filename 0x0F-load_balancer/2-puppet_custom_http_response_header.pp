8a677e1fcaad58f7a6f5fdd8f31f9e01ddefa7ae
coosoti <charlesosoti@gmail.com>
2021-08-24 01:07:40 +0300

    added a puppet manifest to configure custom response header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,12 +8,12 @@ file_line { 'add_header':
   ensure  => 'present',
   require => Package['nginx'],
   path    => '/etc/nginx/sites-available/default',
-  after   => 'root /var/www/html;',
-  line    => 'add_header X-Served-By $HOSTNAME;',
+  after   => "root /var/www/html;",
+  line    => "add_header X-Served-By \"${hostname}\";",
   notify  =>  Service['nginx'],
 }
 
 service { 'nginx':
-  ensure  => running,
-  require => File_line['add_header'],
+  ensure  => 'running',
+  require => file_line['add_header'],
 }
