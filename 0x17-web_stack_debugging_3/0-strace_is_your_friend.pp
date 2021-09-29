84e6c2bda83cc9f1c006429e671302051833a1fa
coosoti <charlesosoti@gmail.com>
2021-09-29 09:00:46 +0300

    added puppet manifests that fixed apache 500 internal server error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix apache 500 internal error by fixing typo in the wordpress settings
+exec { 'fixing typo...':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
