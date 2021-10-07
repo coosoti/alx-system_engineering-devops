40b9b6d544d49d12b181b562a98bfc87e2e20a96
coosoti <charlesosoti@gmail.com>
2021-10-07 15:20:44 +0300

    added puppet manifest that enable a holbern user to login and open a file without any error message

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# enable holberton user to open a file without error
+
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-limt':
+        command => "/bin/sed -i /etc/security/limits.conf -e 's/hard nofile [0-9]\+/hard nofile 10000/g'"
+}
+
+# Increase soft file limit for Holberton user
+exec { 'increase-soft-limit':
+        command => "/bin/sed -i /etc/security/limits.conf -e 's/soft nofile [0-9]\+/soft nofile 10000/g'"
+}
