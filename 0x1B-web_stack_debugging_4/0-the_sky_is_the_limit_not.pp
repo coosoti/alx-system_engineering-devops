210cfde16c745aa45a2bb72d9a6eac712de14b3c
coosoti <charlesosoti@gmail.com>
2021-10-07 15:12:04 +0300

    increased the limit on the restriction of open files limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# increase the number of request to handle
+
+exec { 'increase-open-file-limit':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
